ff112197d2710f494609a4fc75c2918627e3a074
Abhinav Khandekar <100548596+AbhinavKhandekar@users.noreply.github.com>
2022-04-11 13:20:19 +0530

    Simple Overloading Concept Programme

---
+++ 1.6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+//Parameterside Constuctor
+class Complex{
+	int a,b;
+	public:
+		Complex (int x, int y){
+			a=x;
+			b=y;
+		}
+		Complex (int x){
+			a=x;
+			b=0;
+		}
+		void printData(){
+			cout<<"Your Number is : "<<a<<"+"<<b<<"i"<<endl;
+		}
+};
+int main(){
+	Complex c1(4,5);
+	c1.printData();
+	
+	Complex c2(6);
+	c2.printData();
+
+return 0;
+}
+
